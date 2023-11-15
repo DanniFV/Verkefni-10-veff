cf1dfb1dd237a19108e1aaf44819533ff4b90dcf
DanniFV <>
2023-11-15 18:50:20 +0000

    Mamma mia

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Myndir að snúast</p>
+        <img src="Hekla.JPG" className="Hekla-logo" alt="Hekla" />
+        <img src="Matti.jpg" className="Matti-logo" alt="Matti" />
+        <p> Hér eru þau Hekla og Matti að snúast í hringi!!</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
